d7dcf9940897ab8887f1d5e808f3e9c505f3eb78
webfaisalbd <web.faisal.bd@gmail.com>
2022-06-09 22:05:31 +0600

    event object property jemon: target

--- play-with-dom/js/app.js
+++ play-with-dom/js/app.js
@@ -16,7 +16,31 @@ const singleButton = document.getElementById('singleButton');
 // console.log(allButton[0]);
 // array k amra loop calaite pari.
 
-for (const button of allButton){
-    // console.log(button);
+// for (const button of allButton){
+//      console.log(button);  
+// }
+
+
+for(const button of allButton){
+    button.addEventListener('click',(e)=>{
+        // console.log(e.target);
+        // console.log(e.target.parentNode);
+
+        if(e.target.id == 'singleButton'){
+           document.getElementById('myTitle').style.color = 'green';
+        }
+        else {
+            document.getElementById('myTitle').style.color = 'red';
+        }
+    })
 }
+
+// event ekta object , jar vitor target thake.
+// event.target 
+// event.target.parentNode
+
+
+
+
+
 
